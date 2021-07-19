07a3378b6feb47be833e5dcc64139d1bf7582062
dcharlis <dcharlis@pkglobal.com>
2021-07-19 17:25:15 +0530

    Adding files for tv-maze-static

---
+++ precache-manifest.440beb15731af4a572cc4efa6d2cf99d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafe96bddec229323569beacd86a4c8b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee62764b2d45e5094d26",
+    "url": "./static/css/main.a5fc7467.chunk.css"
+  },
+  {
+    "revision": "6ceb8d9a32a504ed61aa",
+    "url": "./static/js/2.1885bb06.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.1885bb06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8100a4b119e4f81f8c22",
+    "url": "./static/js/3.c8ea03f1.chunk.js"
+  },
+  {
+    "revision": "ee62764b2d45e5094d26",
+    "url": "./static/js/main.fb3c6cc5.chunk.js"
+  },
+  {
+    "revision": "77b087f77cdc15f3416e",
+    "url": "./static/js/runtime-main.c9fd28dc.js"
+  }
+]);
